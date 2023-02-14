a2ddf435efd822a5f89596b91f6596df890f4a3d
alessandrini11 <alexandreschuame@gmail.com>
2023-02-14 13:12:28 +0300

    added dependencies

---
+++ src/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(['email'], 'A user is already using this email')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
