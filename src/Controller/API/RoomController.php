bfbc4c3249c7bc385f17460a72117a984c2e8434
alessandrini11 <58996105+alessandrini11@users.noreply.github.com>
2023-04-27 14:21:53 +0100

    get all room

--- src/Controller/API/RoomController.php
+++ src/Controller/API/RoomController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\API;
 
 use App\DTO\RoomRequest;
+use App\model\PaginationModel;
+use App\Repository\RoomRepository;
+use App\Service\PaginationService;
 use App\Service\RoomService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -13,7 +17,8 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class RoomController extends ApiController
 {
     public function __construct(
-        readonly private RoomService $roomService
+        readonly private RoomService $roomService,
+        readonly private PaginationService $paginationService
     )
     {
     }
@@ -25,13 +30,15 @@ class RoomController extends ApiController
         $validationErrors = $validator->validate($roomRequest);
         $this->checkValidationError($validationErrors);
         $room = $this->roomService->create($roomRequest, $this->getUser());
-        return $this->response($room);
+        return $this->response($room, Response::HTTP_CREATED);
     }
 
     #[Route('', name: 'api_room_all', methods: 'GET')]
-    public function all(): JsonResponse
+    public function all(Request $request, RoomRepository $roomRepository): JsonResponse
     {
-        $this->response();
+        $paginationModel = new PaginationModel($request);
+        $array = $this->paginationService->getPaginatedItems($paginationModel, $roomRepository);
+        return $this->response($array);
     }
 
     #[Route('/{id}', name: 'api_room_one', methods: 'GET')]
